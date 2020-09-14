defe2ef2ab8bd3657dda6d7ed56d0e4762b0c46d
Maryana17 <belrandevu@gmail.com>
2020-09-14 21:52:33 +0300

    1-100

--- js/index.js
+++ js/index.js
@@ -1,7 +1,71 @@
 $(document).ready(function(){
 	console.log(1);
-});
+	
+	$(".login-popup .icon-close").click(function(){
+		$(".login-popup").hide();	
+	});
 
+	$(".coin .process").click(function(){
+		var data = $(".coin .data").val() - 0;
+		
+		var coinData = calcCoinText(data);
+		
+		$(".coin .result").text(coinData);
+	});
+	
+	function calcCoinText(number){
+		if (number < 11)
+			return Numbers0To10[number];
+		if (number < 20)
+			return Numbers11To19[number -11];
+		
+	
+		var array = numberToArray(number);
+		var sent = array[2];
+		result = sents [sent - 1];
+		var ten = array[1];
+		result += tens[ten - 2];
+		var numberOne = array[0];
+		if (numberOne > 0){
+			result +=" " + Numbers0To10[numberOne];
+		}
+		return result;
+	}
+
+	function numberToArray(fullNumber){
+		var array = [];
+		
+		while(fullNumber > 0){
+			var number = fullNumber % 10;
+			array.push(number);
+			fullNumber = (fullNumber - number) / 10;
+		}
+		return array;
+	}
+		
+		
+	var Numbers0To10 = ["ноль","один","два", "три","четыре","пять","шесть","семь","восемь","девять","десять"];
+	var Numbers11To19 = ["одиннадцать","двенадцать","тринадцать","четырнадцать","пятнадцать","шестнадцать","семнадцать","восемнадцать","девятнадцать"];
+	var tens = ["двадцать","тридцать","сорок","пятьдесят","шестьдесят","семьдесят","восемьдесят","девяноста"];
+	var sents = ["сто","двести","триста","четыреста","пятьсот","шестьсот","семьсот","восемьсот","девятьсот"]
+	
+	
+	$(".encrypt .process").click(function(){
+		var data = $(".encrypt .data").val() - 0;
+		
+		var encryptData = encryptNumber(data);
+		
+		$(".encrypt .result").text(encryptData);
+		
+	});
+	
+	function encryptNumber(data){
+		var numbersText = ["ноль","один","два", "три","четыре","пять","шесть","семь","восемь","девять"]
+		var encryptData = numbersText[data].length;
+		return encryptData;
+	}
+	
+});
 
 // var condition0 = true;
 // var condition1 = condition0 && false;
